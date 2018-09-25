4b37d364c676a5268b733609ddd4c711ffae4c43
rahulsharma001 <rahul.sharma.976822@gmail.com>
2018-09-25 14:38:17 +0530

    Added Image Upload feature.

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -16,8 +16,21 @@ class RecipeController extends Controller
     }
 
     public function store(Request $request){
-        $responseArr=array();
-        $result= Recipe::create($request->all());
+    $filePath = '';
+    if ($request->user_photo) {
+    $photoName = time().'.'.$request->user_photo->getClientOriginalExtension();
+    $uploadStatus = $request->user_photo->move(public_path('avatar'), $photoName);
+    if ($uploadStatus) {
+        $filePath = public_path('avatar').'/'.$photoName;
+    }
+}
+
+        $responseArr = $request->all();
+    $responseArr['user_photo'] = env('APP_URL').'/avatar/'.$photoName; 
+
+       
+        $result= Recipe::create($responseArr);
+
         if($result){
             $responseArr['errorCode']=0;
             $responseArr['errorMessage']='Successful';
